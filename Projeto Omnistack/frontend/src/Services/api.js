57213cd1aa97ce93b7b098ea7a6e97030d2c1a76
Lucas A <lucwars@hotmail.com>
2020-07-24 14:47:35 -0300

    Removendo Projeto Omnistack

--- Projeto Omnistack/frontend/src/Services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'; //necessita de instalação // CLIENTE HTTP = faz chamada a api do backend e obter as respostas
-
-const api = axios.create({
-    baseURL: 'http://localhost:3333',
-})
-
-export default api;
